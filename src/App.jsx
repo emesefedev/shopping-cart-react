d9b27501ee79c58e27fcc16ca66509851aa131d7
emesefedev <emesefedev@gmail.com>
2025-03-11 17:59:33 +0100

    navbar styled with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
 
-import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-import { ShoppingCartContextProvider, useShoppingCartContext } from "./ShoppingCartContext";
-
-function Navbar() {
-  const {totalProducts} = useShoppingCartContext()
-
-  return (
-    <div className="nav-bar">
-        <p><Link className="link-home" to="/">Home</Link></p>
-        <Link className="shopping-cart-link link-border ml-auto" to="/buy">
-          Shopping Cart
-          { totalProducts > 0 
-            && <div className="shopping-cart-bubble">
-                {totalProducts}
-              </div>
-          }
-        </Link>
-      </div>
-  )
-}
+import { ShoppingCartContextProvider } from "./ShoppingCartContext";
+import Navbar from "./NavBar";
 
 function App() {
-
 
   return (
     <ShoppingCartContextProvider>

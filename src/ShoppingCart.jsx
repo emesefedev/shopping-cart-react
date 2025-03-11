d934f4c5db5011930c3301ab9baaa37ecb85ea39
emesefedev <emesefedev@gmail.com>
2025-03-11 17:23:38 +0100

    show there are no products

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -5,26 +5,33 @@ import { useShoppingCartContext } from "./ShoppingCartContext";
 import ProductEdit from "./ProductEdit";
 
 export default function ShoppingCart() {
-  // const [updateProductsToBuy] = useOutletContext()
+  
   const {products, updateQuantity} = useShoppingCartContext()
+
+  const hasProducts = Object.values(products).length > 0
   
   return (
     <div className="p-32">
       <h1 className="mb-32">Shopping Cart</h1>
-
-      <div className="products-list">
+        <div className="">
+          {hasProducts 
+            ? <> 
+                <div className="products-list">
+                  {Object.values(products).map((product) => {
+                    return <ProductEdit
+                      key={product.id} 
+                      product={product} updateQuantity={updateQuantity}></ProductEdit>
+                    })}
+                </div>
+                <Link to="/check">Check Order</Link>
+              </>
+            : <>
+                <p>There are no products</p>
+                <Link to="/">Find Products</Link>
+              </>
+          }
         
-        {Object.values(products).map((product) => {
-          return <ProductEdit
-            key={product.id} 
-            product={product} updateQuantity={updateQuantity}></ProductEdit>
-        })}
-        
-      </div>
-
-      
-
-      <Link to="/check">Check Order</Link>
+        </div>
     </div>
   )
 }
